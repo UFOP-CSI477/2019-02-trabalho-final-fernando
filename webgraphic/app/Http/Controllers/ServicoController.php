92b338cc4f534291a667a753c7a23834133673dd
Natanael <natanaelemilio@gmail.com>
2019-12-11 15:29:33 -0400

    inclusão cliente, servico, produto - MCR

---
+++ webgraphic/app/Http/Controllers/ServicoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\servico;
+use Illuminate\Http\Request;
+
+class ServicoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\servico  $servico
+     * @return \Illuminate\Http\Response
+     */
+    public function show(servico $servico)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\servico  $servico
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(servico $servico)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\servico  $servico
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, servico $servico)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\servico  $servico
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(servico $servico)
+    {
+        //
+    }
+}
